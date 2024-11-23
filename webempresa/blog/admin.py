a2f243cda356a6acb7660f5ea26de0549381f061
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 04:44:14 +0100

    personalización del panel de administración

--- webempresa/blog/admin.py
+++ webempresa/blog/admin.py
@@ -6,6 +6,11 @@ class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
+    list_display = ('title', 'author', 'published') # Muestra los campos en la lista de entradas.
+    ordering = ('author', 'published') # Ordena las entradas por autor y fecha de publicación.
+    search_fields = ('title','content','author__username', 'categories__name') # Campos por los que se puede buscar.
+    date_hierarchy = 'published' # Jerarquía de fechas.
+    list_filter = ('author__username','categories__name') # Filtros de búsqueda.
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
