e297409d7994ae75964606fa4a3d6ec0c9cb2a13
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 02:52:21 +0100

    edición de settings - creación de superuser

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = "webempresa.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.sqlite3", # Motor de la base de datos
+        "NAME": BASE_DIR / "db.sqlite3", # Ruta de la base de datos
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "es"
 
 TIME_ZONE = "UTC"
 
