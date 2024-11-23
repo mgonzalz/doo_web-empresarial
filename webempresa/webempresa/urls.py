fbd3841f2a18a9b24e1c41a232e76aeb36f4ba27
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 02:57:44 +0100

    prueba de paths - establecer rutas

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('services/', views.services, name="services"),
+    path('store/', views.store, name="store"),
+    path('contact/', views.contact, name="contact"),
+    path('blog/', views.blog, name="blog"),
+    path('sample/', views.sample, name="sample"),
 ]
