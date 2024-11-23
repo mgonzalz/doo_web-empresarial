e706b5a67d2a84f9a91a4fdded33ae08a09a1996
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 04:13:48 +0100

    modificación para eliminar img

--- webempresa/services/models.py
+++ webempresa/services/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+import os
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+
 # Create your models here.
 class Service(models.Model):
     title = models.CharField(max_length=200, verbose_name="Título")
@@ -12,5 +16,22 @@ class Service(models.Model):
         verbose_name = "servicio"
         verbose_name_plural = "servicios"
         ordering = ['-created']
+    
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
+
     def __str__(self):
         return self.title
+
+
+@receiver(post_delete, sender=Service)
+def delete_image_on_object_delete(sender, instance, **kwargs):
+    """
+    Borra la imagen asociada cuando un objeto Project es eliminado.
+    """
+    if instance.image:
+        if os.path.isfile(instance.image.path):  # Verifica si la imagen existe en el sistema de archivos
+            os.remove(instance.image.path)
