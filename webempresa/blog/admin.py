1cb573bae56a7d663070b63a4b123594deba5eeb
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 04:46:05 +0100

    personalización - mostrar modelos en la lista de entradas de admin

--- webempresa/blog/admin.py
+++ webempresa/blog/admin.py
@@ -6,11 +6,13 @@ class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
-    list_display = ('title', 'author', 'published') # Muestra los campos en la lista de entradas.
+    list_display = ('title', 'author', 'published', 'post_categories') # Muestra los campos en la lista de entradas.
     ordering = ('author', 'published') # Ordena las entradas por autor y fecha de publicación.
     search_fields = ('title','content','author__username', 'categories__name') # Campos por los que se puede buscar.
     date_hierarchy = 'published' # Jerarquía de fechas.
     list_filter = ('author__username','categories__name') # Filtros de búsqueda.
+    def post_categories(self, obj):
+        return ", ".join([c.name for c in obj.categories.all().order_by("name")])
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
