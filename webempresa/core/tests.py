13b2dec9ff72a3f0460de5398c2c77188d61e3f3
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 02:59:10 +0100

    rutas completas - creación de test para comprobar

--- webempresa/core/tests.py
+++ webempresa/core/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class TestURL(TestCase):
+    def setUp(self):
+        self.urls = ['home',
+                     'about',
+                     'services',
+                     'store',
+                     'contact',
+                     'blog',
+                     'sample']
+    def test_url(self):
+        for url_name in self.urls:
+            url = reverse(url_name)  # URL para el nombre de la vista si existe en `urls.py`: /url/ .
+            response = self.client.get(url)  # Método GET para la URL: simulación de solicitud HTTP.
+            self.assertEqual(response.status_code, 200) # 200: código de respuesta HTTP de una solicitud exitosa.
