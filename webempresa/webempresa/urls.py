389fb772bbf1f2d2c373ca31785d4ab5d210f4cb
mgonzalz <mgonzgarc@myuax.com>
2024-11-23 03:04:52 +0100

    adaptar url cuando hay distintas apps - creación de `urls.py` en la app

--- webempresa/webempresa/urls.py
+++ webempresa/webempresa/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from core import views
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.home, name="home"),
-    path('about/', views.about, name="about"),
-    path('services/', views.services, name="services"),
-    path('store/', views.store, name="store"),
-    path('contact/', views.contact, name="contact"),
-    path('blog/', views.blog, name="blog"),
-    path('sample/', views.sample, name="sample"),
+    path('', include('core.urls')),
 ]
