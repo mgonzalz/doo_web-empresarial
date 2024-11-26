4a6be5ae25470153ccc80176efc7f1bbddc58663
mgonzalz <mgonzgarc@myuax.com>
2024-11-26 02:12:58 +0100

    creación de grupos, usuarios y pruebas - personalizar `\admin`

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -1,8 +1,14 @@
+from typing import Any
 from django.contrib import admin
+from django.http import HttpRequest
 from .models import Link
 
 # Register your models here.
 class LinkAdmin(admin.ModelAdmin):
-    pass
+    def get_readonly_fields(self, request: HttpRequest, obj: Any) -> tuple:
+        if request.user.groups.filter(name="Personal").exists():
+            return ("key", "name")
+        else:
+            return ()
 
 admin.site.register(Link, LinkAdmin)
